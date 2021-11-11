85d8bbadc9acc5bc5727305d06c6ec4ec4ddcd8e
C.P. Santos <cpsanto@yahoo.com>
2021-11-11 12:42:27 -0700

    reorganized

---
+++ part 2 data cleaning/src/cleaning_utils.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import os
+import glob
+import re
+import sqlalchemy
+import re
+
+def clean_climber_details_from_user_deets(path, pattern):
+    """[takes csvs from folder (path) and combines them into single large dataframe
+    with colnames as columns.  Meant for scraped data]
+
+    Args:
+        path ([string]): [path to folder of csv files]
+        pattern ([string]): [file path of each csv minus the unique name (in case 
+        there is a prefix to filename ie 'climber-info-')]
+
+    Returns:
+        [pandas Dataframe]: [combined csv files into single large dataframe]
+    """
+    all_files = glob.glob(path + "/*.csv")
+    li = []
+    colnames=['location', 'age', 'website', 'sponsors', 'started_climbing', 'occupation', 'other_interests', 'best_climbing_area', 'known_areas', 'height', 'dob'] 
+    for filename in all_files:
+        # pattern = "/Volumes/Backup Plus/climber_deets/climber-info-(.*?).csv"
+        substring = re.search(pattern, filename).group(1)
+        df = pd.read_csv(filename, names=colnames, header=None)
+        df['name'] = substring
+        li.append(df)
+        if len(li)%500 ==0:
+            print (len(li))
+        # for index in range(len(li)):
+        #     if len(li)%500 ==0:
+        #         print(li[index])
+    frame = pd.concat(li)
+    return frame
+
+def combine_ascent_csvs(path):
+    # path = '/Users/cp/Documents/dsi/8a2/8a_scraper/'
+    # extension = 'csv'
+    os.chdir(path)
+    result1 = glob.glob('*.{}'.format('csv'))
+
+    empty_csvs = []
+    for file1 in result1:
+        filesize = os.path.getsize(file1)
+        if filesize == 0:
+            # print(f"{file1} The file is empty: " + str(filesize))
+            empty_csvs.append(file1)
+
+    print(len(empty_csvs))
+    files = [i for i in os.listdir(path) if i.startswith("climber-info")]
+    full_csvs = [i for i in files if i not in empty_csvs]
+    
+    list_df = []
+    count = 0
+    for file in full_csvs:
+        df = pd.read_csv(f'{path}/{file}',header=None,  error_bad_lines=False)
+        list_df.append(df)
+        count +=1
+        if count%500 ==0:
+            print(count)
+    df2 = pd.concat(list_df)
+    df2 = df2.drop_duplicates(keep='last')
+    df2.rename(columns = {0:'ascentId'
+    ,1: 'areaName'
+    ,2: 'areaSlug'
+    ,3: 'cragName'
+    ,4: 'cragSlug'
+    ,5: 'sectorSlug'
+    ,6: 'zlaggableName'
+    ,7: 'zlaggableSlug'
+    ,8: 'countrySlug'
+    ,9: 'userAvatar'
+    ,10: 'userName'
+    ,11: 'userSlug'
+    ,12: 'date'
+    ,13: 'difficulty'
+    ,14: 'gradeIndex'
+    ,15: 'comment'
+    ,16: 'isPrivateComment'
+    ,17:'traditional'
+    ,18:'project'
+    ,19:'isHard'
+    ,20:'isSoft'
+                            
+    ,21: 'firstAscent'
+    ,22: 'secondGo'
+    ,23: 'type'
+    ,24: 'notes'
+    ,25: 'rating'}, inplace = True)
+
+    return df2
+
+# def get_user_ids(csv_file):
+#     df = pd.read_csv(csv_file)
+#     avatar_str_lst = df.userAvatar.unique()
+#     # avatar_str_lst_full = [x for x in avatar_str_lst if x != 'nan']
+#     non_float_lst_avatar_str_lst= []
+#     for i in avatar_str_lst:
+#         if type(i) ==str:
+#             non_float_lst_avatar_str_lst.append(i)
+#     potential_user_nums= []
+    
+#     pattern2 = "gallery/(.*?)\."
+#     for string in non_float_lst_avatar_str_lst:
+#         string= string.lower()
+#         print(string)
+#     #     if "jpg" in string:
+#         substring = re.search(pattern2, string).group(1)
+#         print(substring)
+#         potential_user_nums.append(substring)
+
+#     for i in potential_user_nums:
+#         if i.isnumeric() == True:
+#             print(i)
+
+
+def get_user_ids2(csv_file):
+    df = pd.read_csv(csv_file)
+    df = df.drop_duplicates()
+    df.userAvatar.fillna(value = 'disregard', inplace = True)
+    df['b'] = df['userAvatar'].apply(lambda s:s.split('/')[0].split('.'))
+    df['c'] = df['b'][1].apply(lambda s:s.split('/'))
+    df['num_val'] = df.userAvatar.str.extract('gallery/(\d+).', expand = False)
+    return df
+            
+
+if __name__ == '__main__':
+    # x = clean_climber_details('/Volumes/Backup Plus/climber_deets', "/Volumes/Backup Plus/climber_deets/climber-info-(.*?).csv")
+    # print(x)
+
+    x = climber_ids('/Volumes/Backup Plus/climber_deets')
+    print(x)
+
---
+++ part 2 data cleaning/src/user_id_to_names.py
@@ -0,0 +1,92 @@
+import numpy as np
+import pandas as pd
+import os
+import sqlite3
+import glob
+import datetime
+from tqdm import tqdm
+
+def get_ids(path):
+    """[takes csv files from path folder, checks if csv is empty, converts date columns to days
+    queries database.sqlite for file with same name as route, convert that date column to same date format
+    drops duplicate rows with dates and merges dataframes on date column. Then drops all columns except
+    'user_id', 'userAvatar', 'userName' and returns dataframe and list of csvs that encountered errors]
+
+    Args:
+        path ([string]): [filepath to folder filled with csvs]
+    """
+
+    os.chdir(path)
+    result1 = glob.glob('*.{}'.format('csv'))
+
+    empty_csvs = []
+    for file1 in result1:
+        filesize = os.path.getsize(file1)
+        if filesize == 0:
+            # print(f"{file1} The file is empty: " + str(filesize))
+            empty_csvs.append(file1)
+
+    print(len(empty_csvs))
+#     files = [i for i in os.listdir(path) if i.startswith("climber-info")]
+    files = result1
+    full_csvs = [i for i in files if i not in empty_csvs]
+    print(len(full_csvs))
+
+
+
+    df_lst = []
+    errors_list = []
+    col_list = ['userAvatar', 'userName', 'userSlug', 'date', 'difficulty', 'isHard', 
+       'isSoft', 'type', 'comment', 'traditional', 'project', 'rating', 
+       'userPrivate', 'zlagGradeIndex', 'zlaggableName', 'zlaggableSlug', 
+       'cragSlug', 'cragName', 'countrySlug', 'countryName', 'areaSlug', 
+       'areaName', 'sectorSlug', 'sectorName', 'category', 'recommended', 
+       'firstAscent', 'secondGo', 'isBoltedByMe', 'isOverhang', 'isVertical', 
+       'isSlab', 'isRoof', 'isAthletic', 'isEndurance', 'isCrimpy', 'isCruxy', 
+       'isSloper', 'isTechnical', 'isDanger', 'chipped', 'withKneepad', 
+       'badAnchor', 'badBolts', 'highFirstBolt', 'looseRock', 
+       'badClippingPosition'] 
+    for file_name1 in tqdm(full_csvs[24000:40000]):
+        try:
+            file_name1 = file_name1.replace('\n',"")
+            scraped_df = pd.read_csv(f'/Volumes/Backup Plus/boulder_csv/routes/{file_name1}', names = col_list)
+            # scraped_df = scraped_df.loc[:, ~scraped_df.columns.str.contains('^Unnamed')]
+            scraped_df['date'] = pd.to_datetime(scraped_df['date']).dt.tz_localize(None)
+            scraped_df.date = scraped_df.date.dt.round('1d')
+
+            file_name = scraped_df.zlaggableName.value_counts().index.tolist()[0]
+
+            query1 = f'SELECT * FROM ascent where name = "{file_name}" and climb_type = 0'
+            data = sqlite3.connect('/Users/cp/Documents/dsi/8a_kaggle/database.sqlite')
+            ascents_df = pd.read_sql_query(query1, data)
+            ascents_df['date'] = pd.to_datetime(ascents_df['date'], unit = 's')
+            ascents_df['rec_date'] = pd.to_datetime(ascents_df['date'], unit = 's')
+            ascents_df.date = ascents_df.date.dt.round('1d')
+
+            # ascents_df = ascents_df.loc[:, ~ascents_df.columns.str.contains('^Unnamed')]
+
+            ascents_df = ascents_df.drop_duplicates(subset="date", keep = False)
+            scraped_df = scraped_df.drop_duplicates(subset="date", keep = False)
+
+            new_df = pd.merge(ascents_df, scraped_df, how = 'inner', left_on = 'date', right_on = 'date')
+
+            df_lst.append(new_df)
+
+            compare_df = new_df[['user_id', 'userAvatar', 'userName']]
+        except Exception as error:  
+             
+             
+           
+            errors_list.append(error) 
+    results_df = pd.concat(df_lst)
+    # return compare_df
+    
+    test_df = results_df[['user_id', 'userAvatar', 'userName']]
+    test_df = test_df.drop_duplicates()
+    return test_df, errors_list
+
+if __name__ == '__main__':
+    df, errors_list = get_ids('/Volumes/Backup Plus/boulder_csv/routes/')
+    print(df)
+    print(df.nunique)
+    df.to_csv('/Users/cp/Documents/dsi/github_wikiclimber_public/rock_climbing_project/src/userid_name_avatar9.csv', index = False)
